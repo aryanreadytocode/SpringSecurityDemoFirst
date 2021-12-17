4d46ef1f2b960d85efffb7cb8fccc466f5b689e0
Raj Aryan <arpittaryan2k12@gmail.com>
2021-12-17 09:51:06 +0530

    Initial commit

---
+++ src/main/java/com/aryan/tech/SpringSecurityDemoFirst/SpringSecurityDemoFirstApplication.java
@@ -0,0 +1,13 @@
+package com.aryan.tech.SpringSecurityDemoFirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDemoFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDemoFirstApplication.class, args);
+	}
+
+}
